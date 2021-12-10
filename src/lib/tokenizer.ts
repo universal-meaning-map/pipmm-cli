c2c25252cc459d36b9ac056cba241ca90836d30e
xavi <xavings@gmail.com>
2021-12-10 21:27:21 +0100

    Clear logs

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -93,8 +93,6 @@ export default class Tokenizer {
   ): Promise<string> => {
     //folder/foamid|property/subProperty --> mid:iid/tiid/subProperty
     let runs = wikilink.split("|");
-    console.log("runs")
-    console.log(runs)
     let exp = "";
     let frontRuns = runs[0].split("/");
     if (frontRuns.length == 1) {
