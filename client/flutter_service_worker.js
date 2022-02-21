7f6650c2e809b1eed4e09c3321113677a74fe07c
xavi <xavings@gmail.com>
2022-02-21 19:04:05 +0100

    Client upgrade: Setting note viewer as default

--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "03e700e5a83770ec6a50de5460de639c",
-"index.html": "75e845d84129a08b348c2a84bdbe6527",
-"/": "75e845d84129a08b348c2a84bdbe6527",
-"main.dart.js": "fd132c96073fe0e6b61b92a2809dc3d1",
+"index.html": "7fa0633ad33f9484e7298051e4d942ee",
+"/": "7fa0633ad33f9484e7298051e4d942ee",
+"main.dart.js": "e4d9ec34b6d9d2ac44edbec777c56cc5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
